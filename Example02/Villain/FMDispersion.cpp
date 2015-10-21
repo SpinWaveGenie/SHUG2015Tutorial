aa4b1fe51c4d6af493b13830ccfa3940c1226014
Steven Hahn <hahnse@ornl.gov>
2015-10-21 18:12:06 -0400

    Rework Villain model example.

--- Example02/Villain/FMDispersion.cpp
+++ Example02/Villain/FMDispersion.cpp
@@ -42,10 +42,9 @@ int main()
     builder.addInteraction(interactions.getExchange("metaJ",-eta*J,name3,name3,0.8,1.2));
     builder.addInteraction(interactions.getExchange("gammaJ",gamma*J,name1,name3,0.8,1.2));
     
-    //Vector3 zhat(0.0,0.0,1.0);
-    //builder.addInteraction(interactions.getMagneticField("B",B/2.0,zhat,name1));
-    //builder.addInteraction(interactions.getMagneticField("B",B/2.0,zhat,name3));
-
+    Vector3 zhat(0.0,0.0,1.0);
+    builder.addInteraction(interactions.getMagneticField("B",B,zhat,name1));
+    builder.addInteraction(interactions.getMagneticField("B",B,zhat,name3));
 
     SpinWave test = builder.createElement();
     
@@ -56,32 +55,26 @@ int main()
     ThreeVectors<double> kPoints = Line.getPoints();
     
     SpinWaveDispersion dispersion;
-    dispersion.setFilename("AFMChain.txt");
+    dispersion.setFilename("VillainFMPhase.txt");
     dispersion.setGenie(test);
     dispersion.setPoints(kPoints);
     
     dispersion.save();
     
-    
-    double kx(0.0),ky(0.0);
-    double R1K = sqrt(pow(eta+1,2)*pow(1-cos(kx),2)+4.0*pow(gamma*cos(ky),2));
-    double term = B + 2*gamma + (eta-1.0)*(cos(kx)-1.0);
-    cout << term+R1K << " " << term-R1K << endl;
-    cout << (R1K-2*gamma*cos(ky))/R1K << " " << (R1K+2*gamma*cos(ky))/R1K << endl;
+    std::cout << "Analytical Results: For comparison with VillainFMPhase.txt\n";
+    std::cout << "H, K, L, E1, E+, E-, I+, I-\n";
 
-    
-    kx = M_PI;
-    R1K = sqrt(pow(eta+1,2)*pow(1-cos(kx),2)+4.0*pow(gamma*cos(ky),2));
-    term = B + 2*gamma + (eta-1.0)*(cos(kx)-1.0);
-    cout << term+R1K << " " << term-R1K << endl;
-    cout << (R1K-2*gamma*cos(ky))/R1K << " " << (R1K+2*gamma*cos(ky))/R1K << endl;
-
-    
-    kx = 2.0*M_PI;
-    R1K = sqrt(pow(eta+1,2)*pow(1-cos(kx),2)+4.0*pow(gamma*cos(ky),2));
-    term = B + 2*gamma + (eta-1.0)*(cos(kx)-1.0);
-    cout << term+R1K << " " << term-R1K << endl;
-    cout << (R1K-2*gamma*cos(ky))/R1K << " " << (R1K+2*gamma*cos(ky))/R1K << endl;
+    double ky = 0.0;
+    for(const auto& point : kPoints)
+    {
+      cout << point.get<0>() << " " << point.get<1>() << " " << point.get<2>() << " ";
+      double kx = 2.0*M_PI*point.get<0>();
+      double ky = 2.0*M_PI*point.get<0>();
+      double R1K = sqrt(pow(eta+1,2)*pow(1-cos(kx),2)+4.0*pow(gamma*cos(ky),2));
+      double term = B + 2.0*gamma + (eta-1.0)*(cos(kx)-1.0);
+      cout << term+R1K << " " << term-R1K << " ";
+      cout << (R1K-2.0*gamma*cos(ky))/R1K << " " << (R1K+2.0*gamma*cos(ky))/R1K << endl;
+    }
 
     return 0;
 }
--- Example02/Villain/SC1Dispersion.cpp
+++ Example02/Villain/SC1Dispersion.cpp
@@ -6,11 +6,9 @@
 using namespace std;
 using namespace SpinWaveGenie;
 
-SpinWaveBuilder getBuilder(double theta_a, double theta_b)
+SpinWaveBuilder getBuilder(double theta_a, double theta_b, double gamma)
 {
-    double gamma,eta,J;
-    
-    gamma = 1.0;
+    double eta,J;
     eta = 4.0;
     J = 1.0;
     
@@ -62,13 +60,14 @@ SpinWaveBuilder getBuilder(double theta_a, double theta_b)
     
 }
 
-double myfunc(const std::vector<double> &x, std::vector<double> & /*grad*/, void * /*my_func_data*/)
+double myfunc(const std::vector<double> &x, std::vector<double> & /*grad*/, void * data)
 {
-    SpinWaveBuilder builder = getBuilder(x[0],x[1]);
+    double *gamma = reinterpret_cast<double*>(data);
+    SpinWaveBuilder builder = getBuilder(x[0],x[1],*gamma);
     return builder.getEnergy();
 }
 
-std::vector<double> findAngles()
+std::vector<double> findAngles(double gamma)
 {
     
     double minf = 0.0;
@@ -81,9 +80,9 @@ std::vector<double> findAngles()
     opt.set_lower_bounds(lb);
     
     opt.set_ftol_abs(1.0e-13);
-    opt.set_maxeval(5000);
+    opt.set_maxeval(50000000);
     
-    opt.set_min_objective(myfunc,NULL);
+    opt.set_min_objective(myfunc,&gamma);
     opt.optimize(thetaphi,minf);
     
     return thetaphi;
@@ -92,26 +91,33 @@ std::vector<double> findAngles()
 int main()
 {
     
-    vector<double> x = findAngles();
-    cout << x[0] << " " << x[1] << endl;
     
-    SpinWaveBuilder builder = getBuilder(x[0], x[1]);
-    SpinWave test = builder.createElement();
+    for(int i = 0; i<5;++i)
+    {
+      vector<double> x = findAngles(static_cast<double>(i));
+      cout << x[0] << " " << x[1]-M_PI_2 << endl;
     
-    PointsAlongLine Line;
-    Line.setFirstPoint(0.0,0.0,0.0);
-    Line.setFinalPoint(0.0,1.0,0.0);
-    Line.setNumberPoints(101);
-    ThreeVectors<double> kPoints = Line.getPoints();
+      SpinWaveBuilder builder = getBuilder(x[0], x[1],static_cast<double>(i));
+      SpinWave test = builder.createElement();
     
-    SpinWaveDispersion dispersion;
-    dispersion.setFilename("SC1Chain.txt");
-    dispersion.setGenie(test);
-    dispersion.setPoints(kPoints);
+      PointsAlongLine Line;
+      Line.setFirstPoint(1.0,0.0,0.0);
+      Line.setFinalPoint(0.0,0.0,0.0);
+      Line.setNumberPoints(101);
+      ThreeVectors<double> points100 = Line.getPoints();
     
-    dispersion.save();
+      Line.setFirstPoint(0.0,0.0,0.0);
+      Line.setFinalPoint(0.0,1.0,0.0);
+      Line.setNumberPoints(101);
+      ThreeVectors<double> points010 = Line.getPoints();
     
-
+      SpinWaveDispersion dispersion;
+      dispersion.setFilename("SC1Chain_"+std::to_string(i)+".txt");
+      dispersion.setGenie(test);
+      dispersion.setPoints(points100);
+      dispersion.setPoints(points010);
     
+      dispersion.save();
+    }
     return 0;
 }
