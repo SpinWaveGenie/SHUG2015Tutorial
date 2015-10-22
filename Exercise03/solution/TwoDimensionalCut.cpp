1008c7f56327ee8896c128d6486c94ce9b0e75ed
Steven Hahn <hahnse@ornl.gov>
2015-10-22 17:55:33 -0400

    fix example03

--- Exercise03/solution/TwoDimensionalCut.cpp
+++ Exercise03/solution/TwoDimensionalCut.cpp
@@ -3,73 +3,7 @@
 using namespace std;
 using namespace SpinWaveGenie;
 
-void alongKAxis()
-{
-    SpinWave SW = createModel();
-    
-    PointsAlongLine Line;
-    Line.setFirstPoint(2.0,-1.5,-3.0);
-    Line.setFinalPoint(2.0, 1.5,-3.0);
-    Line.setNumberPoints(201);
-    ThreeVectors<double> kPoints = Line.getPoints();
-    
-    Energies energies(0.0, 80.0, 201);
-    
-    TwoDimGaussian resinfo;
-    resinfo.a = 1109.0;
-    resinfo.b = 0.0;
-    resinfo.c = 0.48;
-    resinfo.tol = 1.0e-1;
-    resinfo.direction = Vector3(0.0,1.0,0.0);
-    
-    unique_ptr<SpinWavePlot> res(new TwoDimensionResolutionFunction(resinfo, SW, energies));
-    
-    HKLDirections direction;
-    direction.addDirection(0.0,0.0,1.0,0.2);
-    direction.addDirection(1.0,0.0,0.0,0.2);
-    //direction.addDirection(0.0,1.0,0.0,0.05);
-    
-    unique_ptr<SpinWavePlot> asdf(new IntegrateAxes(std::move(res),direction,1.0e-1));
-
-    TwoDimensionalCut twodimcut;
-    twodimcut.setFilename("YFeO3_2_K_m3");
-    twodimcut.setPlotObject(move(asdf));
-    twodimcut.setPoints(kPoints);
-    twodimcut.save();
-
-    Line.setFirstPoint(2.0,-1.5,-2.0);
-    Line.setFinalPoint(2.0, 1.5,-2.0);
-    Line.setNumberPoints(201);
-    kPoints = Line.getPoints();
-
-    twodimcut.setFilename("YFeO3_2_K_m2");
-    twodimcut.setPoints(kPoints);
-    twodimcut.save();
-}
-
-void volumeCalculation()
-{
-  SpinWave SW = createModel();
-
-  Energies energies(0.0, 80.0, 201);
-
-  PointsOnAPlane plane;
-  plane.setProjections({{0.0, 1.0, 0.0}}, {{0.0, 0.0, 1.0}});
-  plane.setExtents({0.0,M_PI, 201}, {0.0,2.0*M_PI, 201});
-
-  OneDimensionalFactory factory;
-  auto gauss = factory.getGaussian(1.0, 1.0e-5);
-
-  unique_ptr<SpinWavePlot> res(new EnergyResolutionFunction(move(gauss), SW, energies));
-
-  ThreeDimensionalCut twodimcut;
-  twodimcut.setFilename("FMcut");
-  twodimcut.setPlotObject(move(res));
-  twodimcut.setPoints(plane);
-  twodimcut.save();
-}
-
-void alongLAxis()
+int main()
 {
     SpinWave SW = createModel();
 
@@ -110,13 +44,5 @@ void alongLAxis()
     twodimcut.setFilename("YFeO3_3_1_L");
     twodimcut.setPoints(kPoints);
     twodimcut.save();
-}
-
-int main()
-{
-  // alongKAxis();
-  // slower
-  // alongLAxis();
-  volumeCalculation();
     return 0;
 }
